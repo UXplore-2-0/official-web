f9bc98af7f3691266c2be468ba9639f4875b3457
dilumin <77558016+dilumin@users.noreply.github.com>
2024-02-06 20:28:47 +0530

    refactor: Clean entire project for reboot

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,15 @@
-import Login from "./components/layout/Login";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Navigate } from "react-router-dom";
-import Register from "./components/layout/Register";
-import React, { Fragment } from "react";
-import Landing from "./components/layout/Landing";
-import Navbar from "./components/layout/Navbar";
-import EmailVerify from "./components/layout/EmailVerify";
-import TeamRegister from "./components/layout/TeamRegister";
-import Pending from "./components/layout/Pending";
-import { TeamProvider } from "./components/layout/TeamContext";
+import React from 'react'
+import Navbar from "./layout/NavBar/Navbar"
+import Home from "./pages/Home/Home"
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      route: "dashboard",
-    };
-  }
-  setRoute = (route) => {
-    this.setState({
-      route: route,
-    });
-  };
-
-  render() {
-    return (
-      <Router>
-        <Fragment>
-          <Navbar />
-          <TeamProvider>
-            <Routes>
-              <Route exact path="/dashboard" element={<Landing />} />
-              {/* <Route exact path="/login" element={<Login />} /> */}
-              {/* <Route
-                exact
-                path="/register"
-                element={<Register setRoute={this.setRoute} />}
-              /> */}
-              <Route path="/activate/:token" element={<EmailVerify />} />
-              <Route exact path="/addMembers" element={<TeamRegister />} />
-              <Route path="/pending" element={<Pending />} />
-              <Route
-                path="*"
-                element={<Navigate to="/dashboard" replace={true} />}
-              />
-            </Routes>
-          </TeamProvider>
-        </Fragment>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    
+    </>
+  )
 }
 
-export default App;
+export default App
--- client/src/components/constants.js
+++
@@ -1,2 +0,0 @@
-export const API_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:3001/api/v1";
