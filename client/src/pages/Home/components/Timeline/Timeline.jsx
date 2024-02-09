dd7ada9663c99f9cc7419182e832658a8081bb5e
Deshitha Gallage <deshithagallage623@gmail.com>
2024-02-09 23:12:07 +0530

    Added basic components of the timeline

--- client/src/pages/Home/components/Timeline/Timeline.jsx
+++ client/src/pages/Home/components/Timeline/Timeline.jsx
@@ -1,8 +1,90 @@
 import React from 'react'
+import './Timeline.css'
 
 function Timeline() {
   return (
-    <div>Timeline</div>
+    <>
+    <div className='title'>
+      <h1>Timeline</h1>
+      <p>The timeline of the Mora UXplore 2.0</p>
+    </div>
+
+    <div className='timeline'>
+      
+      <div className='timeline-container'>
+        <div>
+          <p>April 25</p>
+          <h1>Awareness Session</h1>
+          <p>Via Zoom</p>
+        </div>
+      </div>
+
+      <div className='timeline-container'>
+        <div>
+          <p>April 29</p>
+          <h1>Workshop 01</h1>
+          <p>Via Zoom</p>
+        </div>
+      </div>
+
+      <div className='timeline-container'>
+        <div>
+          <p>April 30</p>
+          <h1>Round 01</h1>
+          <p>Through Mora UXplore Website</p>
+        </div>
+      </div>
+
+      <div className='timeline-container'>
+        <div>
+          <p>May 06</p>
+          <h1>Workshop 02</h1>
+          <p>Via Zoom</p>
+        </div>
+      </div>
+
+      <div className='timeline-container'>
+        <div>
+          <p>May 07</p>
+          <h1>Round 02</h1>
+          <p>Through Mora UXplore Website</p>
+        </div>
+      </div>
+
+      <div className='timeline-container'>
+        <div>
+          <p>June 03</p>
+          <h1>Workshop 03</h1>
+          <p>At University Premises</p>
+        </div>
+      </div>
+
+      <div className='timeline-container'>
+        <div>
+          <p>June 04</p>
+          <h1>Round 03</h1>
+          <p>Through Mora UXplore Website</p>
+        </div>
+      </div>
+
+      <div className='timeline-container'>
+        <div>
+          <p>June 17</p>
+          <h1>Final Designathon</h1>
+          <p>At University Premises</p>
+        </div>
+      </div>
+
+      <div className='timeline-container'>
+        <div>
+          <p>June 17</p>
+          <h1>Award Ceremony</h1>
+          <p>At University Premises</p>
+        </div>
+      </div>
+
+    </div>
+    </>
   )
 }
 
