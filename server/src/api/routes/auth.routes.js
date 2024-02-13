ca4bd3f5d5878b9c017bec3194a3aed082f2bdb3
Shavin Anjitha <shavinanjithachandrawansha@gmail.com>
2024-02-14 00:23:53 +0530

    Add signUpAdmin function to handle admin account creation

--- server/src/api/routes/auth.routes.js
+++ server/src/api/routes/auth.routes.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const {
   signUpTeam,
+  signUpAdmin,
   login,
   verifyTeam,
 } = require('../controllers/auth.controller');
+const { auth, admin } = require('../middleware/auth');
 
 // create a new Router for the auth routes
 const router = express.Router();
 
+router.post('/signup/admin', auth, admin, signUpAdmin);
 router.post('/signup', signUpTeam);
 router.post('/verify/:team_name/:token', verifyTeam);
 router.post('/login', login);
