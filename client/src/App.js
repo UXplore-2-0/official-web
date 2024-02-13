819e638f0524cb93eefa86259c958a4aedb8b33d
Verosha Kriyanjala <veroshakriyanjala32@gmail.com>
2024-02-13 16:21:18 +0530

    set to my background

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import Navbar from "./layout/NavBar/Navbar"
-import Home from "./pages/Home/Home"
+import React from "react";
+import Navbar from "./layout/NavBar/Navbar";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <Home />
-    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
-import Main from './components/Main/Main'
-import Guidlines from './components/Guidlines/Guidlines'
-import Timeline from './components/Timeline/Timeline'
-import Sponsorships from './components/Sponsorships/Sponsorships'
-import Contacts from './components/Contacts/Contacts'
-
+import React from "react";
+import Main from "./components/Main/Main";
+import Guidlines from "./components/Guidlines/Guidlines";
+import Timeline from "./components/Timeline/Timeline";
+import Sponsorships from "./components/Sponsorships/Sponsorships";
+import Contacts from "./components/Contacts/Contacts";
 
 function Home() {
   return (
     <>
-      <Main/>
-      <Guidlines/>
-      <Timeline/>
-      <Sponsorships/>
-      <Contacts/>
+      {/* <Main/> */}
+      {/* <Guidlines/> */}
+      {/* <Timeline/> */}
+      {/* <Sponsorships/> */}
+      <Contacts />
     </>
-    
-  )
+  );
 }
 
 export default Home;
