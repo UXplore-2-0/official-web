d7dbda480ceb31cb2ea741a2f5cd340effdd08ff
ISKumarasingha <sanjanakumarasingha1@gmail.com>
2024-02-16 23:00:07 +0530

    Adding Layers

--- client/src/pages/Home/components/Contacts/Contacts.jsx
+++ client/src/pages/Home/components/Contacts/Contacts.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import Bubble from './ContactUs/Bubble Animation/Bubble.jsx'
-import ContactUsPage from './ContactUs/ContactUsPage'
+import React from "react";
+import Bubble from "./ContactUs/Bubble Animation/Bubble.jsx";
+import ContactUsPage from "./ContactUs/ContactUsPage";
 
 function Contacts() {
   return (
-    <div>
-      <Bubble />
-      <ContactUsPage />
+    <div class="layer-container relative">
+      <div class="z-10 relative">
+        <ContactUsPage />
+      </div>
+      <div class="z-20 relative">
+        <Bubble />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contacts
+export default Contacts;
