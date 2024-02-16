9307d8fb789ebf623e9c2aa913d261a81307a88d
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-02-16 13:05:38 +0530

    Guidelines styles fixed

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,11 @@ import Home from "./pages/Home/Home";
 import Guidlines from "./pages/Home/components/Guidlines/Guidlines";
 
 function App() {
-  return <Guidlines />;
+  return (
+    <div>
+      <Guidlines />
+    </div>
+  );
 }
 
 export default App;
--- client/src/pages/Home/components/Guidlines/Guidlines.jsx
+++ client/src/pages/Home/components/Guidlines/Guidlines.jsx
@@ -1,41 +1,122 @@
-import React, { useEffect } from "react";
-import { gsap } from "gsap";
+import React from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
-function Guidlines() {
-  const splitText = (text) => {
-    return text
-      .split("")
-      .map((item, index) =>
-        item === " " ? (
-          <div className="word">&nbsp;</div>
-        ) : (
-          <div className="word">{item}</div>
-        )
-      );
-  };
-
-  useEffect(() => {
-    const chars = document.querySelectorAll("#quote span");
-
-    gsap.from(chars, {
-      duration: 2,
-      opacity: 1,
+export default function Guidlines() {
+  useGSAP(() => {
+    gsap.from(".char", {
+      duration: 0.8,
+      scrollTrigger: {
+        trigger: ".char",
+        start: "top 90%",
+        markers: false,
+      },
+      opacity: 0,
       scale: 0,
-      y: 80,
+      y: 100,
       rotationX: 180,
       transformOrigin: "0% 50% -50",
       ease: "back",
-      stagger: 0.01,
+      stagger: 0.015,
+    });
+
+    gsap.from(".points-block", {
+      duration: 0.8,
+      opacity: 0,
+      x: -100,
+      ease: "ease-in-out",
+      delay: 0.5,
+      stagger: 0.05,
+    });
+    gsap.from(".characters", {
+      scrollTrigger: {
+        trigger: ".characters",
+        start: "-10% 90%",
+        end: "bottom 56%",
+        scrub: true,
+        markers: false,
+      },
+      opacity: 0,
+      stagger: 0.5,
+      x: 20,
+      transformOrigin: "60% 50% -100",
+      ease: "ease-in",
     });
   }, []);
 
+  const sentence1 = "sdfvsdefdsvdse  dsbfkbka bsakcb hjsb kc";
+
+  const sentence2 = "Hello, GSAP is awesome gsd! ugasbuzdb ushncihn hnduiaHNXS";
+  const sentence3 =
+    "Hello, GSAP is awesome hjbaszjba! BAIBIU HIHXA UHOHisbx jnhaho";
+  const sentence4 =
+    "Hello, GSAP is awesome hjbaszjba! BAIBIU HIHXA UHOHisbx jnhaho";
+  const sentence5 =
+    "Hello, GSAP is awesome hjbaszjba! BAIBIU HIHXA UHOHisbx jnhaho";
+  const sentence6 =
+    "Hello, GSAP is awesome hjbaszjba! BAIBIU HIHXA UHOHisbx jnhaho";
+  const sentence7 =
+    "Hello, GSAP is awesome hjbaszjba! BAIBIU HIHXA UHOHisbx jnhaho";
+
+  const array = [
+    sentence1,
+    sentence2,
+    sentence3,
+    sentence4,
+    sentence5,
+    sentence6,
+    sentence7,
+  ];
+
+  gsap.registerPlugin(ScrollTrigger);
   return (
-    <div id="quote" className="text-lg flex-col">
-      {splitText(
-        "SplitText makes it easy to break apart the text in an HTML element so that each character, word, and/or line is wrapped in its own div tag."
-      )}
+    <div>
+      <img src="https://images.unsplash.com/photo-1464925257126-6450e871c667?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
+      <img src="https://images.unsplash.com/photo-1464925257126-6450e871c667?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
+      <img src="https://images.unsplash.com/photo-1464925257126-6450e871c667?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
+      <div
+        className="w-[100%] h-[100%] flex flex-col bg-cover justify-between py-11 sm:flex-row-reverse"
+        style={{
+          backgroundImage:
+            "url('https://rare-gallery.com/thumbs/1180321-sea-artwork-underwater-light-ocean-darkness.jpg')",
+        }}
+      >
+        <div className="text-[45px] text-white rounded-xl mb-3 ml-5 p-3 guidelines flex items-center mr-8 md:text-[96px] sm:text-[45] w-full sm:justify-end sm:ml-0">
+          {"GUIDELINES".split("").map((characters, index) => {
+            return characters === " " ? (
+              <span key={index}>&nbsp;</span>
+            ) : (
+              <span key={index} className="characters font-medium">
+                {characters}
+              </span>
+            );
+          })}
+        </div>
+        <div className="bg-white/0 mb-3 points-block backdrop-blur-md w-full sm:max-w-[80%]">
+          {array.map((sentence, index) => {
+            return (
+              <div key={index} className="ml-5  text-white  p-3 flex-wrap">
+                <div
+                  className="flex ml-2 text-sm text-white/40 flex-wrap sm:text-base md:text-lg"
+                  style={{ fontFamily: "rubik,sans-serif" }}
+                >
+                  {sentence.split("").map((char, index) => {
+                    return char === " " ? (
+                      <span key={index}>&nbsp;</span>
+                    ) : (
+                      <span key={index} className="char">
+                        {char}
+                      </span>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <img src="https://images.unsplash.com/photo-1464925257126-6450e871c667?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
     </div>
   );
 }
-
-export default Guidlines;
