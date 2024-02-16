517b3013c0986f1576b4c9e4ef46f098618cffc0
ISKumarasingha <sanjanakumarasingha1@gmail.com>
2024-02-16 23:12:20 +0530

    Refactor App.jsx and Contacts.jsx*** ***Update background image in Bubble.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Navbar from "./layout/NavBar/Navbar";
 import Home from "./pages/Home/Home";
-import "./App.css";
-import ContactUsPage from "./pages/Home/components/Contacts/ContactUs/ContactUsPage";
 import Contacts from "./pages/Home/components/Contacts/Contacts";
+import "./App.css";
 
 function App() {
   return (
     <>
-      {/* <ContactUsPage /> */}
       <Contacts />
     </>
   );
--- client/src/pages/Home/components/Contacts/Contacts.jsx
+++ client/src/pages/Home/components/Contacts/Contacts.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import Bubble from "./ContactUs/Bubble Animation/Bubble.jsx";
 import ContactUsPage from "./ContactUs/ContactUsPage";
+import BackGround from "./ContactUs/images/Deepsea.svg";
 
 function Contacts() {
   return (
     <div class="layer-container relative">
       <div class="z-10 relative">
-        <ContactUsPage />
+        <img
+          src={BackGround}
+          alt="BackGround Image"
+          className="w-fill h-fill"
+        />
       </div>
       <div class="z-20 relative">
+        <ContactUsPage />
+      </div>
+      <div class="z-30 relative">
         <Bubble />
       </div>
     </div>
