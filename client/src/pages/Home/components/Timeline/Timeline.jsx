48ca44d84be0395996c468f99923128b22d3626b
Deshitha Gallage <deshithagallage623@gmail.com>
2024-02-17 13:31:25 +0530

    Change the dimension of timeline to vertical

--- client/src/pages/Home/components/Timeline/Timeline.jsx
+++ client/src/pages/Home/components/Timeline/Timeline.jsx
@@ -11,178 +11,110 @@ gsap.registerPlugin(ScrollTrigger);
 function Timeline() {
 
   useGSAP(() => {
-    gsap.utils.toArray(".inner-container").forEach((div, index) => {
-      gsap.from(div, {
-        opacity: 0,
-        x: index % 2 === 0 ? "100" : "-100",
-        y: 25,
-        duration: 1.6,
-        scrollTrigger: {
-          trigger: div,
-          start: "top 90%",
-          end: "bottom 60%",
-          scrub: true,
-          toggleActions: "play none none none",
-          markers: true,
-        },
-      });
-    });
-
-    gsap.utils.toArray(".out-bar").forEach((div, index) => {
+    gsap.utils.toArray(".inner-container").forEach((div) => {
       gsap.from(div, {
         opacity: 0,
         scale: 0,
+        y: 50,
         duration: 1.6,
         scrollTrigger: {
           trigger: div,
-          start: "top 90%",
-          end: "bottom 60%",
+          start: "top 95%",
+          end: "top 70%",
           scrub: true,
           toggleActions: "play none none none",
           markers: true,
         },
       });
     });
+
+    gsap.from('.timeline-title h1', {
+      opacity: 0.5,
+      y: 50,
+      duration: 1,
+      scrollTrigger: {
+        trigger: '.timeline-title h1',
+        start: "top 90%",
+        end: "bottom 70%",
+        scrub: true,
+        toggleActions: "play none none none",
+        markers: true,
+      },
+    });
+
+    gsap.from('.timeline-title p', {
+      opacity: 0,
+      y: 35,
+      duration: 1.6,
+      scrollTrigger: {
+        trigger: '.timeline-title p',
+        start: "top 90%",
+        end: "bottom 70%",
+        scrub: true,
+        toggleActions: "play none none none",
+        markers: true,
+      },
+    });
+
+
   });
 
   return (
-    <div className="timeline-backdrop font-sans text-white p-8">
+    
+    <div className="timeline-backdrop text-white px-10 py-10 md:py-24">
 
-    <div className='timeline-title flex flex-col items-center'>
-      <h1 className='mb-1 text-4xl md:text-6xl lg:text-7xl xl:text-8xl font-bold'>Timeline</h1>
-      <p className='text-center text-[#0d71fc] lg:text-lg'>The timeline of the Mora UXplore 2.0</p>
+      <div className='timeline-title flex flex-col items-center'>
+        <h1 className='mb-1 text-4xl md:text-6xl lg:text-7xl xl:text-8xl font-bold'>Timeline</h1>
+        <p className='text-center text-stone-200 text-sm md:text-base lg:text-lg'>Unraveling the Dynamic Journey through Time in the Mora UXplore 2.0 Evolution</p>
+      </div>
+
+      <div className='timeline flex flex-col gap-y-[2em] items-center mx-auto my-[3em] md:my-[5em]'>
+          
+        <div className='inner-container shadow-lg bg-transparent backdrop-blur-xl border-2 border-solid border-[#888] rounded-2xl w-[225px] md:w-[375px] p-[0.8em] md:p-[1em] lg:p-[1.2em] xl:p-[1.5em]'>
+          <p className='text-xs font-normal md:text-base'>February 25</p>
+          <h1 className='text-xl md:text-3xl font-bold'>Registration Open</h1>
+          <p className='text-xs font-normal md:text-base pt-[3px]'>Through Mora UXplore 2.0 Official Website</p>
+        </div>
+
+        <div className='inner-container shadow-lg bg-transparent backdrop-blur-xl border-2 border-solid border-[#888] rounded-2xl w-[225px] md:w-[375px] p-[0.8em] md:p-[1em] lg:p-[1.2em] xl:p-[1.5em]'>
+          <p className='text-xs font-normal md:text-base'>March 2</p>
+          <h1 className='text-xl md:text-3xl font-bold'>Awareness Session</h1>
+          <p className='text-xs font-normal md:text-base pt-[3px]'>Via Zoom</p>
+        </div>
+
+        <div className='inner-container shadow-lg bg-transparent backdrop-blur-xl border-2 border-solid border-[#888] rounded-2xl w-[225px] md:w-[375px] p-[0.8em] md:p-[1em] lg:p-[1.2em] xl:p-[1.5em]'>
+          <p className='text-xs font-normal md:text-base'>March 9</p>
+          <h1 className='text-xl md:text-3xl font-bold'>Registration Closing</h1>
+          <p className='text-xs font-normal md:text-base pt-[3px]'>Through Mora UXplore 2.0 Official Website</p>
+        </div>
+
+        <div className='inner-container shadow-lg bg-transparent backdrop-blur-xl border-2 border-solid border-[#888] rounded-2xl w-[225px] md:w-[375px] p-[0.8em] md:p-[1em] lg:p-[1.2em] xl:p-[1.5em]'>
+          <p className='text-xs font-normal md:text-base'>March 16</p>
+          <h1 className='text-xl md:text-3xl font-bold'>Workshop 01</h1>
+          <p className='text-xs font-normal md:text-base pt-[3px]'>Via Zoom</p>
+        </div>
+
+        <div className='inner-container shadow-lg bg-transparent backdrop-blur-xl border-2 border-solid border-[#888] rounded-2xl w-[225px] md:w-[375px] p-[0.8em] md:p-[1em] lg:p-[1.2em] xl:p-[1.5em]'>
+          <p className='text-xs font-normal md:text-base'>March 30</p>
+          <h1 className='text-xl md:text-3xl font-bold'>Workshop 02</h1>
+          <p className='text-xs font-normal md:text-base pt-[3px]'>Via Zoom</p>
+        </div>
+
+        <div className='inner-container shadow-lg bg-transparent backdrop-blur-xl border-2 border-solid border-[#888] rounded-2xl w-[225px] md:w-[375px] p-[0.8em] md:p-[1em] lg:p-[1.2em] xl:p-[1.5em]'>
+          <p className='text-xs font-normal md:text-base'>April 9</p>
+          <h1 className='text-xl md:text-3xl font-bold'>Semi Finals</h1>
+          <p className='text-xs font-normal md:text-base pt-[3px]'>At University Premises</p>
+        </div>
+
+        <div className='inner-container shadow-lg bg-transparent backdrop-blur-xl border-2 border-solid border-[#888] rounded-2xl w-[225px] md:w-[375px] p-[0.8em] md:p-[1em] lg:p-[1.2em] xl:p-[1.5em]'>
+          <p className='text-xs font-normal md:text-base'>April 27</p>
+          <h1 className='text-xl md:text-3xl font-bold'>Final Designathon & Award Ceremony</h1>
+          <p className='text-xs font-normal md:text-base pt-[3px]'>At University Premises</p>
+        </div>
+          
+      </div>
     </div>
-
-    <div className='timeline flex flex-col items-center mx-auto my-[5em]'>
-      
-      <div className='timeline-container relative max-w-[16em] py-[2em] transform
-                      md:max-w-[25.15em] md:border-solid md:border-[#888] md:odd:border-l-[3px] md:even:border-r-[3px] md:odd:pl-[3em] md:even:pr-[3em] md:odd:translate-x-[9em] md:even:-translate-x-[9em]
-                      lg:min-w-[25em] lg:even:pr-[3em] lg:odd:translate-x-[12.425em] lg:even:-translate-x-[12.425em]'>
-        
-        <div className='out-bar flex absolute bg-[#888] h-[4em] w-[3px] left-1/2 top-0 transform -translate-y-1/2
-                        md:h-[3px] md:w-[3em] md:left-0 md:top-1/2'></div>
-        <div className='inner-container min-w-[15em] md:max-w-[25em] border-2 border-solid border-[#888] rounded-2xl p-[0.7em] md:p-[0.8em] lg:p-[1.2em] xl:p-[1.5em] bg-transparent backdrop-blur-xl'>
-          <p className='text-xs font-normal md:text-sm xl:text-lg'>April 25</p>
-          <h1 className='text-xl md:text-2xl lg:text-3xl font-bold'>Awareness Session</h1>
-          <p className='text-xs font-normal md:text-sm xl:text-lg'>Via Zoom</p>
-        </div>
-        
-      </div>
-
-      <div className='timeline-container relative max-w-[16em] py-[2em] transform
-                      md:max-w-[25.15em] md:border-solid md:border-[#888] md:odd:border-l-[3px] md:even:border-r-[3px] md:odd:pl-[3em] md:even:pr-[3em] md:odd:translate-x-[9em] md:even:-translate-x-[9em]
-                      lg:min-w-[25em] lg:even:pr-[3em] lg:odd:translate-x-[12.425em] lg:even:-translate-x-[12.425em]'>
-        
-        <div className='out-bar flex absolute bg-[#888] h-[4em] w-[3px] left-1/2 top-0 transform -translate-y-1/2
-                        md:h-[3px] md:w-[3em] md:left-full md:ml-[-3em] md:top-1/2'></div>
-        <div className='inner-container min-w-[15em] md:max-w-[25em] border-2 border-solid border-[#888] rounded-2xl p-[0.7em] md:p-[0.8em] lg:p-[1.2em] xl:p-[1.5em] bg-transparent backdrop-blur-xl'>
-          <p className='text-xs font-normal md:text-sm xl:text-lg'>April 29</p>
-          <h1 className='text-xl md:text-2xl lg:text-3xl font-bold'>Workshop 01</h1>
-          <p className='text-xs font-normal md:text-sm xl:text-lg'>Via Zoom</p>
-        </div>
-        
-      </div>
-
-      <div className='timeline-container relative max-w-[16em] py-[2em] transform
-                      md:max-w-[30.15em] md:border-solid md:border-[#888] md:odd:border-l-[3px] md:even:border-r-[3px] md:odd:pl-[3em] md:even:pr-[3em] md:odd:translate-x-[9em] md:even:-translate-x-[9em]
-                      lg:min-w-[25em] lg:even:pr-[3em] lg:odd:translate-x-[12.425em] lg:even:-translate-x-[12.425em]'>
-        
-        <div className='out-bar flex absolute bg-[#888] h-[4em] w-[3px] left-1/2 top-0 transform -translate-y-1/2
-                        md:h-[3px] md:w-[3em] md:left-0 md:top-1/2'></div>
-        <div className='inner-container min-w-[15em] md:max-w-[25em] border-2 border-solid border-[#888] rounded-2xl p-[0.7em] md:p-[0.8em] lg:p-[1.2em] xl:p-[1.5em] bg-transparent backdrop-blur-xl'>
-          <p className='text-xs font-normal md:text-sm xl:text-lg'>April 30</p>
-          <h1 className='text-xl md:text-2xl lg:text-3xl font-bold'>Round 01</h1>
-          <p className='text-xs font-normal md:text-sm xl:text-lg'>Through Mora UXplore Website</p>
-        </div>
-        
-      </div>
-
-      <div className='timeline-container relative max-w-[16em] py-[2em] transform
-                      md:max-w-[25.15em] md:border-solid md:border-[#888] md:odd:border-l-[3px] md:even:border-r-[3px] md:odd:pl-[3em] md:even:pr-[3em] md:odd:translate-x-[9em] md:even:-translate-x-[9em]
-                      lg:min-w-[25em] lg:even:pr-[3em] lg:odd:translate-x-[12.425em] lg:even:-translate-x-[12.425em]'>
-        
-        <div className='out-bar flex absolute bg-[#888] h-[4em] w-[3px] left-1/2 top-0 transform -translate-y-1/2
-                        md:h-[3px] md:w-[3em] md:left-full md:ml-[-3em] md:top-1/2'></div>
-        <div className='inner-container min-w-[15em] md:max-w-[25em] border-2 border-solid border-[#888] rounded-2xl p-[0.7em] md:p-[0.8em] lg:p-[1.2em] xl:p-[1.5em] bg-transparent backdrop-blur-xl'>
-          <p className='text-xs font-normal md:text-sm xl:text-lg'>May 06</p>
-          <h1 className='text-xl md:text-2xl lg:text-3xl font-bold'>Workshop 02</h1>
-          <p className='text-xs font-normal md:text-sm xl:text-lg'>Via Zoom</p>
-        </div>
-        
-      </div>
-
-      <div className='timeline-container relative max-w-[16em] py-[2em] transform
-                      md:max-w-[25.15em] md:border-solid md:border-[#888] md:odd:border-l-[3px] md:even:border-r-[3px] md:odd:pl-[3em] md:even:pr-[3em] md:odd:translate-x-[9em] md:even:-translate-x-[9em]
-                      lg:min-w-[25em] lg:even:pr-[3em] lg:odd:translate-x-[12.425em] lg:even:-translate-x-[12.425em]'>
-        
-        <div className='out-bar flex absolute bg-[#888] h-[4em] w-[3px] left-1/2 top-0 transform -translate-y-1/2
-                        md:h-[3px] md:w-[3em] md:left-0 md:top-1/2'></div>
-        <div className='inner-container min-w-[15em] md:max-w-[25em] border-2 border-solid border-[#888] rounded-2xl p-[0.7em] md:p-[0.8em] lg:p-[1.2em] xl:p-[1.5em] bg-transparent backdrop-blur-xl'>
-          <p className='text-xs font-normal md:text-sm xl:text-lg'>May 07</p>
-          <h1 className='text-xl md:text-2xl lg:text-3xl font-bold'>Round 02</h1>
-          <p className='text-xs font-normal md:text-sm xl:text-lg'>Through Mora UXplore Website</p>
-        </div>
-        
-      </div>
-
-      <div className='timeline-container relative max-w-[16em] py-[2em] transform
-                      md:max-w-[25.15em] md:border-solid md:border-[#888] md:odd:border-l-[3px] md:even:border-r-[3px] md:odd:pl-[3em] md:even:pr-[3em] md:odd:translate-x-[9em] md:even:-translate-x-[9em]
-                      lg:min-w-[25em] lg:even:pr-[3em] lg:odd:translate-x-[12.425em] lg:even:-translate-x-[12.425em]'>
-        
-        <div className='out-bar flex absolute bg-[#888] h-[4em] w-[3px] left-1/2 top-0 transform -translate-y-1/2
-                        md:h-[3px] md:w-[3em] md:left-full md:ml-[-3em] md:top-1/2'></div>
-        <div className='inner-container min-w-[15em] md:max-w-[25em] border-2 border-solid border-[#888] rounded-2xl p-[0.7em] md:p-[0.8em] lg:p-[1.2em] xl:p-[1.5em] bg-transparent backdrop-blur-xl'>
-          <p className='text-xs font-normal md:text-sm xl:text-lg'>June 03</p>
-          <h1 className='text-xl md:text-2xl lg:text-3xl font-bold'>Workshop 03</h1>
-          <p className='text-xs font-normal md:text-sm xl:text-lg'>At University Premises</p>
-        </div>
-        
-      </div>
-
-      <div className='timeline-container relative max-w-[16em] py-[2em] transform
-                      md:max-w-[25.15em] md:border-solid md:border-[#888] md:odd:border-l-[3px] md:even:border-r-[3px] md:odd:pl-[3em] md:even:pr-[3em] md:odd:translate-x-[9em] md:even:-translate-x-[9em]
-                      lg:min-w-[25em] lg:even:pr-[3em] lg:odd:translate-x-[12.425em] lg:even:-translate-x-[12.425em]'>
-        
-        <div className='out-bar flex absolute bg-[#888] h-[4em] w-[3px] left-1/2 top-0 transform -translate-y-1/2
-                        md:h-[3px] md:w-[3em] md:left-0 md:top-1/2'></div>
-        <div className='inner-container min-w-[15em] md:max-w-[25em] border-2 border-solid border-[#888] rounded-2xl p-[0.7em] md:p-[0.8em] lg:p-[1.2em] xl:p-[1.5em] bg-transparent backdrop-blur-xl'>
-          <p className='text-xs font-normal md:text-sm xl:text-lg'>June 04</p>
-          <h1 className='text-xl md:text-2xl lg:text-3xl font-bold'>Semi Final</h1>
-          <p className='text-xs font-normal md:text-sm xl:text-lg'>Through Mora UXplore Website</p>
-        </div>
-        
-      </div>
-
-      <div className='timeline-container relative max-w-[16em] py-[2em] transform
-                      md:max-w-[25.15em] md:border-solid md:border-[#888] md:odd:border-l-[3px] md:even:border-r-[3px] md:odd:pl-[3em] md:even:pr-[3em] md:odd:translate-x-[9em] md:even:-translate-x-[9em]
-                      lg:min-w-[25em] lg:even:pr-[3em] lg:odd:translate-x-[12.425em] lg:even:-translate-x-[12.425em]'>
-        
-        <div className='out-bar flex absolute bg-[#888] h-[4em] w-[3px] left-1/2 top-0 transform -translate-y-1/2
-                        md:h-[3px] md:w-[3em] md:left-full md:ml-[-3em] md:top-1/2'></div>
-        <div className='inner-container min-w-[15em] md:max-w-[25em] border-2 border-solid border-[#888] rounded-2xl p-[0.7em] md:p-[0.8em] lg:p-[1.2em] xl:p-[1.5em] bg-transparent backdrop-blur-xl'>
-          <p className='text-xs font-normal md:text-sm xl:text-lg'>June 17</p>
-          <h1 className='text-xl md:text-2xl lg:text-3xl font-bold'>Final Designathon</h1>
-          <p className='text-xs font-normal md:text-sm xl:text-lg'>At University Premises</p>
-        </div>
-        
-      </div>
-
-      <div className='timeline-container relative max-w-[16em] py-[2em] transform
-                      md:max-w-[25.15em] md:border-solid md:border-[#888] md:odd:border-l-[3px] md:even:border-r-[3px] md:odd:pl-[3em] md:even:pr-[3em] md:odd:translate-x-[9em] md:even:-translate-x-[9em]
-                      lg:min-w-[25em] lg:even:pr-[3em] lg:odd:translate-x-[12.425em] lg:even:-translate-x-[12.425em]'>
-        
-        <div className='out-bar flex absolute bg-[#888] h-[4em] w-[3px] left-1/2 top-0 transform -translate-y-1/2
-                        md:h-[3px] md:w-[3em] md:left-0 md:top-1/2'></div>
-        <div className='inner-container min-w-[15em] md:max-w-[25em] border-2 border-solid border-[#888] rounded-2xl p-[0.7em] md:p-[0.8em] lg:p-[1.2em] xl:p-[1.5em] bg-transparent backdrop-blur-xl'>
-          <p className='text-xs font-normal md:text-sm xl:text-lg'>June 17</p>
-          <h1 className='text-xl md:text-2xl lg:text-3xl font-bold'>Award Ceremony</h1>
-          <p className='text-xs font-normal md:text-sm xl:text-lg'>At University Premises</p>
-        </div>
-        
-      </div>
-
-    </div>
-    </div>
+    
   )
 }
 
