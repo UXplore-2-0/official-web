f07a828fc0fc1db64c9fac3fd82e58487a8933cf
dilumin <77558016+dilumin@users.noreply.github.com>
2024-02-17 22:59:33 +0530

    Update Tailwind configuration and package dependencies

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,jsx}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
