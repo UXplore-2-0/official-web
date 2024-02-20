c1d8a9e72f763786533c9b7a5867d3cad535c3ce
dilumin <77558016+dilumin@users.noreply.github.com>
2024-02-20 20:33:37 +0530

    Bug fix

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Main from './components/Main/Main'
+import Main from './components/Hero/hero'
 import Guidlines from './components/Guidlines/Guidlines'
 import Timeline from './components/Timeline/Timeline'
 import Sponsorships from './components/Sponsorships/Sponsorships'
@@ -12,9 +12,9 @@ function Home() {
     <>
       <Main/>
       <Intro />
-      
-      <Timeline/>
       <Guidlines/>
+      <Timeline/>
+      
       <Sponsorships/>
       <Contacts/>
     </>
