b4caa10f4db484dd2319907becd0d90177875325
Deshitha Gallage <deshithagallage623@gmail.com>
2024-02-21 09:40:53 +0530

    Added background gradient and glass morphism effect

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -4,12 +4,12 @@ import SeaImage from './images/Sea.png'
 
 function Login() {
     return (
-        <div className='relative bg-gradient-to-br from-[#0E172A] to-[#1E455E]'>
+        <div className='relative' style={{ background: 'linear-gradient(to bottom right, #182B44 5%, #1E3855 15%, #0F2132 40%, #1E455E 95%)' }}>
             <div className='absolute z-10 w-full flex justify-end'>
                 <img src={SeaImage} alt='Sea Image' className='object-cover h-screen w-1/2' />
             </div>
             <div className='relative z-20 text-white flex flex-col items-center justify-center w-full min-h-screen'>  
-                <div className='bg-transparent backdrop-blur-2xl shadow-lg flex flex-col items-center justify-center w-[85%] sm:w-[78%] md:w-[70%] lg:w-[55%] py-8 lg:py-10 rounded-2xl font-poppins my-5'>
+                <div className='bg-white/10 backdrop-blur-2xl shadow-lg flex flex-col items-center justify-center w-[85%] sm:w-[78%] md:w-[70%] lg:w-[55%] py-8 lg:py-10 rounded-2xl font-poppins my-5'>
                     <img src={Logo} alt='Mora UXplore 2.0 Logo' className='w-28 sm:w-32 lg:w-36'/>
                     <div className='flex flex-col items-center justify-center my-6'>
                         <h1 className='text-2xl tracking-widest'>WELCOME BACK</h1>
