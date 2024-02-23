04def11951ff03ee0280846dbc0bdedce7c37bfa
dilumin <77558016+dilumin@users.noreply.github.com>
2024-02-23 14:34:30 +0530

    Register and Log in Seccesfully merged

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import Home from "./pages/Home/Home";
 import Guidlines from "./pages/Home/components/Guidlines/Guidlines";
 import Timeline from "./pages/Home/components/Timeline/Timeline";
 import Intro from "./pages/Home/components/Intro/Intro";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 import "./App.css";
 
 function App() {
