b04660ba0be3ad9824a9ce6d44cbeb95ecef666a
Sajith <sajithanuradha860@gmail.com>
2024-02-23 16:42:59 +0530

    Finished the Submission Details UI

--- client/src/pages/Dashboard/AdminDashboard/Pages/SubmissionDetails.jsx
+++ client/src/pages/Dashboard/AdminDashboard/Pages/SubmissionDetails.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Searchbar from '../Components/Searchbar'
 
 function SubmissionDetails() {
 
@@ -19,44 +20,49 @@ function SubmissionDetails() {
 
     return (
         <div className="justify-between ">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-black uppercase dark:text-gray-400 bg-blue-600">
-                    <tr>
-                        <th scope="col" className="px-6 py-3">
-                            <button className="text-white">Team Name</button>
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            <button className="text-white">Email</button>
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            <button className="text-white">University Name</button>
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            <button className="text-white">Link</button>
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            <button className="text-white">Date</button>
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            <button className="text-white">Submit</button>
-                        </th>
-                    </tr>
-                </thead>
-                <tbody className='bg-blue-950'>
-                    {Team_Details.map((team, index) => (
-                        <tr key={index}>
-                            <td className="px-6 py-3 cursor-pointer" onClick={() => showMemberDetails(team.teamName)}>{team.teamName}</td>
-                            <td className="px-6 py-3">{team.email}</td>
-                            <td className="px-6 py-3">{team.universityName}</td>
-                            <td className="px-6 py-3">{team.Link}</td>
-                            <td className="px-6 py-3">{team.Date}</td>
-                            <td className="px-6 py-3">
-                                {team.submit ? <span className="bg-green-500 text-white px-2 py-1 rounded-full text-xs">Submitted</span> : <span className="bg-red-500 text-white px-2 py-1 rounded-full text-xs">Not Submitted</span>}
-                            </td>
+            <div className='justify-center '>
+                <Searchbar />
+            </div>
+            <div className='mt-5'>
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-black uppercase dark:text-gray-400 bg-blue-600">
+                        <tr>
+                            <th scope="col" className="px-6 py-3">
+                                <button className="text-white">Team Name</button>
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                <button className="text-white">Email</button>
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                <button className="text-white">University Name</button>
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                <button className="text-white">Link</button>
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                <button className="text-white">Date</button>
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                <button className="text-white">Submit</button>
+                            </th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody className='bg-blue-950'>
+                        {Team_Details.map((team, index) => (
+                            <tr key={index}>
+                                <td className="px-6 py-3 cursor-pointer" onClick={() => showMemberDetails(team.teamName)}>{team.teamName}</td>
+                                <td className="px-6 py-3">{team.email}</td>
+                                <td className="px-6 py-3">{team.universityName}</td>
+                                <td className="px-6 py-3">{team.Link}</td>
+                                <td className="px-6 py-3">{team.Date}</td>
+                                <td className="px-6 py-3">
+                                    {team.submit ? <span className="bg-green-500 text-white px-2 py-1 rounded-full text-xs">Submitted</span> : <span className="bg-red-500 text-white px-2 py-1 rounded-full text-xs">Not Submitted</span>}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
